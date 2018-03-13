db8228fc495253bdf6c9d2d1011befae98f894b7
Rico <r.montulet@student.maastrichtuniversity.nl>
2018-03-13 13:13:28 +0100

    You can now put preprocessed into the project directory and it wont get pushed to git, but we also dont need full paths anymore when plotting or reading the pickle files

--- convert_data.py
+++ convert_data.py
@@ -1,7 +1,7 @@
 import scipy.io as sio
 import pickle
 
-#############################################
+########n#####################################
 ## Preperation for this script:
 ## 1. Clone github reporsitory into folder of your choice
 ## 2. Download data ( https://unishare.nl/index.php/s/Hk6kYh88aIeVd0Z )
@@ -11,30 +11,30 @@ import pickle
 print("CURRENTLY ONLY USING SINGLE FILE")
 convert = input('Have you aleady performed the conversion? (y/n) ')
 if convert == 'n':
-    mat_contents = sio.loadmat('../preprocessed/datathetaOscTLbyTimeV_FAC001.mat')
+	mat_contents = sio.loadmat('preprocessed/datathetaOscTLbyTimeV_FAC001.mat')
 
-    X_memory = mat_contents['dataMatM']
-    y_memory = mat_contents['simVecM']
+	X_memory = mat_contents['dataMatM']
+	y_memory = mat_contents['simVecM']
 
-    X_perc = mat_contents['dataMatP']
-    y_perc = mat_contents['simVecP']
+	X_perc = mat_contents['dataMatP']
+	y_perc = mat_contents['simVecP']
 
-    with open('../preprocessed/X_memory.pkl', 'wb') as fp:
-        pickle.dump(X_memory, fp)
-    with open('../preprocessed/y_memory.pkl', 'wb') as fp:
-        pickle.dump(y_memory, fp)
-    with open('../preprocessed/X_perc.pkl', 'wb') as fp:
-        pickle.dump(X_perc, fp)
-    with open('../preprocessed/y_perc.pkl', 'wb') as fp:
-        pickle.dump(y_perc, fp)
+	with open('preprocessed/X_memory.pkl', 'wb') as fp:
+		pickle.dump(X_memory, fp)
+	with open('preprocessed/y_memory.pkl', 'wb') as fp:
+		pickle.dump(y_memory, fp)
+	with open('preprocessed/X_perc.pkl', 'wb') as fp:
+		pickle.dump(X_perc, fp)
+	with open('preprocessed/y_perc.pkl', 'wb') as fp:
+		pickle.dump(y_perc, fp)
 
 load_pickle = input('Would you like to load the pickles? (y/n)')
 if load_pickle == 'y':
-    with open ('../preprocessed/X_memory.pkl', 'rb') as fp:
-        X_memory = pickle.load(fp)
-    with open ('../preprocessed/y_memory.pkl', 'rb') as fp:
-        y_memory = pickle.load(fp)
-    with open ('../preprocessed/X_perc.pkl', 'rb') as fp:
-        X_perc = pickle.load(fp)
-    with open ('../preprocessed/y_perc.pkl', 'rb') as fp:
-        y_perc = pickle.load(fp)
+	with open('preprocessed/X_memory.pkl', 'rb') as fp:
+		X_memory = pickle.load(fp)
+	with open('preprocessed/y_memory.pkl', 'rb') as fp:
+		y_memory = pickle.load(fp)
+	with open('preprocessed/X_perc.pkl', 'rb') as fp:
+		X_perc = pickle.load(fp)
+	with open('preprocessed/y_perc.pkl', 'rb') as fp:
+		y_perc = pickle.load(fp)
--- plot_leads.py
+++ plot_leads.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 
 num_of_leads_to_plot = 2
 
-with open ('C:\FILES\MAASTRICHT UNIVERSITY\RESEARCH PROJECT 2\X_memory.pkl', 'rb') as fp:
-    X_memory = pickle.load(fp)
-with open ('C:\FILES\MAASTRICHT UNIVERSITY\RESEARCH PROJECT 2\y_memory.pkl', 'rb') as fp:
-    y_memory = pickle.load(fp)
-with open ('C:\FILES\MAASTRICHT UNIVERSITY\RESEARCH PROJECT 2\X_perc.pkl', 'rb') as fp:
-    X_perc = pickle.load(fp)
-with open ('C:\FILES\MAASTRICHT UNIVERSITY\RESEARCH PROJECT 2\y_perc.pkl', 'rb') as fp:
-    y_perc = pickle.load(fp)
+with open('preprocessed/X_memory.pkl', 'rb') as fp:
+	X_memory = pickle.load(fp)
+with open('preprocessed/y_memory.pkl', 'rb') as fp:
+	y_memory = pickle.load(fp)
+with open('preprocessed/X_perc.pkl', 'rb') as fp:
+	X_perc = pickle.load(fp)
+with open('preprocessed/y_perc.pkl', 'rb') as fp:
+	y_perc = pickle.load(fp)
 
 for i in range(0, num_of_leads_to_plot):
-    plt.plot(X_memory[i])
+	plt.plot(X_memory[i])
 
 plt.show()
