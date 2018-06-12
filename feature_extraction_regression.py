ea1d82b7bdd4006e11ee1c5e7b1520745adb55ae
cedricoeldorf <cedric.oeldorf@gmail.com>
2018-06-12 16:09:34 +0200

    added multithreading attempt, no data yet

--- feature_extraction_regression.py
+++ feature_extraction_regression.py
@@ -6,31 +6,33 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 from scipy import stats
 from copy import deepcopy
+from multiprocessing import Pool, Manager, cpu_count, Queue
 target = [0,1,0,1,0,1,1,0,1,0]
 feature_names = ['f1','f2','f3']
-#############
+#############3
+# We have a lead. All trials, in each trial split into time segments
+# first: get features of every time segment: for trial, for segment, append
+# get: a list of features in a list of segments for each trial
 
 
-###########################
-## Calculates slopes of features and returns slopesself.
-###########################
+## This is one time segmented array of features (one lead)
+## Run this for a specific lead over all trials
+## Start with array that has n features in k segments
+
+
 def get_slopes(feature_names):
+    #for every lead
 
     slopes = []
-    # For every lead
     for i in range(125):
-
-        trial_slopes = []
         # for every trial
+        trial_slopes = []
         for j in range(10):
-
-            # Fake data
             # 3 faetures in 9 segments
             features = np.asarray([[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)],[np.random.uniform(-1,1, size = 3)]])
             features = features.T
             n_features = len(features)
 
-            # Get slopes for current trial
             slope = []
             for k in range(n_features):
                 f = features[k].ravel()
@@ -39,54 +41,39 @@ def get_slopes(feature_names):
                 slope.append(m)
             trial_slopes.append(slope)
 
-        # Append trial slopes
+        #print(trial_slopes[0])
         slopes.append(trial_slopes)
     return slopes
 
-#######################
-## Average the slopes for each feature over all trials to check for significance
-## OPTION: Remove features whos average slopes are close to 0
 def average_slope(slopes, feature_names, removal = False, bottom_thresh = 0, top_thresh = 0):
-
+    # INCOMPLETE
     mean_slopes = []
-
-    # For every lead
     for z in range(len(slopes)):
 
         # take lead, transpose in order to compare every target
         slopes[z] = np.asarray(slopes[z]).T
         sl = []
-
-        ## get average of every features slopes for every trial
         for x in range(len(slopes[z])):
             av_sl = slopes[z][x].mean()
             sl.append(av_sl)
         mean_slopes.append(sl)
 
-    ## With defined thresholds, remove features both from the slopes
-    ## and the feature name list
     if removal == True:
         final_slopes = deepcopy(mean_slopes)
         temp_slopes = deepcopy(mean_slopes)
         temp_slopes = np.asarray(temp_slopes).T
-
         for i in range(len(temp_slopes)):
             print("MIN: " + str(temp_slopes[i].min()))
             print("MAX: " + str(temp_slopes[i].max()))
             index = []
-
             if (temp_slopes[i].min() > bottom_thresh) and (temp_slopes[i].max() < top_thresh):
                 final_slopes = np.delete(final_slopes, i, 0)
                 index.append(i)
-
-        # delete shitty features from names list
         del feature_names[i]
         mean_slopes = deepcopy(final_slopes)
 
     return mean_slopes, feature_names
 
-#####################
-## Get correlation of features to target
 def get_importances(slopes,target, feature_names):
 
     #############################
@@ -111,11 +98,10 @@ def get_importances(slopes,target, feature_names):
         for x in range(len(slopes[z])):
             acc.append(accuracy_score(target,slopes[z][x]))
 
+        #print(len(acc))
+        #print(importance)
         importance.append(acc)
     return importance
-
-###############################################################
-## RUN ALL
 
 slopes = get_slopes(feature_names)
 s = deepcopy(slopes)
@@ -123,3 +109,32 @@ mean_slope, feature_names = average_slope(s, feature_names, removal = True, bott
 print(feature_names)
 s = deepcopy(slopes)
 important = get_importances(s,target, feature_names)
+
+
+'''######################################
+## attempt to multithread get_slopes function '''
+
+def extract_multithreaded_basic(X):
+    pool = Pool(cpu_count())
+
+    m = Manager()
+    queue = m.Queue()  # create queue to save all the results
+
+    tasks = []
+    for lead in range(X.shape[0]):
+        for trial in range(X.shape[1]):
+            for segment in range(X.shape[2]):
+                for feature in range(X.shape[3]):
+                    n_feat = X.shape[3]
+                    feat = segment.T[feature]
+                    tasks.append([feat, n_feat, queue])
+    print(len(tasks))
+    pool.map(execute, tasks)  # create the results
+    return queue
+
+
+def execute(args):
+	f, n_feat, queue = args[0], args[1], args[2]
+    x = list(range(0,n_feat))
+	m,b = np.polyfit(x, f, 1)
+	queue.put((m, b))
