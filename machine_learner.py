54f17712f29f459c2870ceff2948a47844ac578f
Rico <r.montulet@student.maastrichtuniversity.nl>
2018-06-16 15:19:15 +0200

    cleaning features is implemented and removing empty trials

--- machine_learner.py
+++ machine_learner.py
@@ -140,17 +140,25 @@ def make_feature_pickles(patient_name, patient_data, segment_patient_data, bin_s
 		features, feature_names = extract_multithreaded_basic(patient_data['eeg_m'])
 	else:
 		features, feature_names = extract_basic(patient_data['eeg_m'])
-	pickle.dump((features, feature_names), open('preprocessed/pickle/features_mem_{}_{}_{}_{}_{}_{}_{}_{}.pkl'.format(patient_name, segment_patient_data, bin_size, with_overlap, overlap_step_size, extract_frequency_data, frequency_band_mem, frequency_band_perc), 'wb'))
+	pickle.dump((features, feature_names),
+				open('preprocessed/pickle/features_mem_{}_{}_{}_{}_{}_{}_{}_{}.pkl'.format(patient_name, segment_patient_data, bin_size, with_overlap, overlap_step_size, extract_frequency_data, frequency_band_mem, frequency_band_perc),
+					 'wb'))
 
 	if use_multithreading_if_available:
 		features, feature_names = extract_multithreaded_basic(patient_data['eeg_p'])
 	else:
 		features, feature_names = extract_basic(patient_data['eeg_p'])
-	pickle.dump((features, feature_names), open('preprocessed/pickle/features_perc_{}_{}_{}_{}_{}_{}_{}_{}.pkl'.format(patient_name, segment_patient_data, bin_size, with_overlap, overlap_step_size, extract_frequency_data, frequency_band_mem, frequency_band_perc), 'wb'))
+	pickle.dump((features, feature_names),
+				open('preprocessed/pickle/features_perc_{}_{}_{}_{}_{}_{}_{}_{}.pkl'.format(patient_name, segment_patient_data, bin_size, with_overlap, overlap_step_size, extract_frequency_data, frequency_band_mem, frequency_band_perc),
+					 'wb'))
+
+
+def compress(data, selectors):
+	return [d for d, s in zip(data, selectors) if s]
 
 
 def filter_features(features, y_mem):
-	binned = len(features.shape) == 3
+	binned = len(features.shape) == 4
 	y = y_mem.tolist()
 	f = features.tolist()
 
@@ -158,20 +166,37 @@ def filter_features(features, y_mem):
 		print('not implemented')
 	else:
 		ind_to_remove = []
+		features_to_keep = []
 		n_leads = len(f)
+		i = 0
+		j = 0
+		while len(features_to_keep) == 0:
+			features_to_keep = [True for _ in range(len(f[i][j]))]
+			if i < n_leads - 1:
+				i += 1
+			else:
+				raise Exception('didnt find a lead with more than 0 features... RIP')
 		ymem_per_lead = [y.copy() for _ in range(n_leads)]
 		for lead in range(n_leads):
 			n_trials = len(f[lead])
 			for trial in range(n_trials):
-				s = len(f[lead][trial])
-				if s == 0:
+				feature_vector = f[lead][trial]
+				for i, feat in enumerate(feature_vector):
+					if str(feat) == 'nan':
+						features_to_keep[i] = False
+				if len(feature_vector) == 0:
 					ind_to_remove.append((lead, trial))
-
 		for i in sorted(ind_to_remove, reverse=True):
 			del ymem_per_lead[i[0]][i[1]]
 			del f[i[0]][i[1]]
-
+		before = len(f[0][0])
+		for lead in range(n_leads):
+			n_trials = len(f[lead])
+			for trial in range(n_trials):
+				f[lead][trial] = compress(f[lead][trial], features_to_keep)
+		print('Removed {} out of {} features'.format(before - len(f[0][0]), before))
 		return np.array(f), np.array(ymem_per_lead)
+
 
 def main():
 	##############
@@ -204,7 +229,7 @@ def main():
 	patient_data = load_raw(patient_name)
 	if run_prediction:
 		pickle_file = 'preprocessed/pickle/features_mem_{}_{}_{}_{}_{}_{}_{}_{}.pkl'.format(patient_name, segment_patient_data, bin_size, with_overlap, overlap_step_size, extract_frequency_data, frequency_band_mem, frequency_band_perc)
-		print(pickle_file)
+		print('using file :', pickle_file)
 
 		try:
 			features, feature_names = pickle.load(open(pickle_file, 'rb'))
@@ -228,8 +253,6 @@ def main():
 	else:
 		make_feature_pickles(patient_name, patient_data, segment_patient_data, bin_size, with_overlap, overlap_step_size, use_multithreading_if_available, extract_frequency_data, frequency_band_mem, frequency_band_perc)
 
-
-
 	'''
 	10 - fold
 	acc per lead: [0.6238095238095238, 0.6247619047619047, 0.565, 0.605, 0.5549999999999999, 0.5323809523809524, 0.5702380952380952, 0.5178571428571428, 0.41809523809523813, 0.545, 0.6209523809523809, 0.5402380952380952, 0.4819047619047619, 0.5607142857142857, 0.5638095238095239, 0.48071428571428576, 0.48666666666666664, 0.5507142857142857, 0.5347619047619048, 0.5354761904761904, 0.5016666666666667, 0.6133333333333333, 0.5742857142857142, 0.5995238095238096, 0.5742857142857143, 0.45690476190476187, 0.5011904761904763, 0.5354761904761905, 0.5900000000000001, 0.6064285714285715, 0.5597619047619048, 0.5609523809523809, 0.5321428571428571, 0.5311904761904762, 0.5357142857142857, 0.5345238095238095, 0.5404761904761906, 0.5604761904761906, 0.5595238095238095, 0.5707142857142857, 0.6835714285714285, 0.63, 0.6695238095238095, 0.5419047619047619, 0.5002380952380954, 0.49000000000000005, 0.47690476190476183, 0.4980952380952381, 0.4909523809523809, 0.5214285714285715, 0.5397619047619048, 0.6057142857142856, 0.5009523809523809, 0.5361904761904761, 0.5364285714285715, 0.48238095238095235, 0.5107142857142858, 0.5804761904761906, 0.5704761904761906, 0.5454761904761904, 0.5073809523809524, 0.49071428571428577, 0.5254761904761904, 0.4597619047619048, 0.5702380952380952, 0.5092857142857142, 0.545952380952381, 0.5502380952380952, 0.5611904761904762, 0.5902380952380952, 0.5595238095238095, 0.5897619047619047, 0.5164285714285713, 0.47095238095238096, 0.5302380952380952, 0.5464285714285714, 0.5064285714285715, 0.5502380952380952, 0.49071428571428566, 0.5752380952380952, 0.5357142857142857, 0.595, 0.6357142857142857, 0.536904761904762, 0.48047619047619056, 0.6402380952380953, 0.5995238095238096, 0.5552380952380952, 0.6009523809523809, 0.5611904761904761, 0.5902380952380952, 0.4969047619047619, 0.5804761904761906, 0.5421428571428573, 0.5407142857142857, 0.6297619047619047, 0.6211904761904762, 0.485952380952381, 0.4978571428571429, 0.536904761904762, 0.6549999999999999, 0.5116666666666666, 0.6047619047619047, 0.555952380952381, 0.5902380952380952, 0.5842857142857143, 0.5561904761904761, 0.5747619047619048, 0.5766666666666667, 0.6261904761904762, 0.6354761904761904, 0.549047619047619, 0.6788095238095238, 0.6121428571428571, 0.5561904761904762, 0.595, 0.5640476190476191, 0.5169047619047619, 0.5609523809523809, 0.5114285714285715, 0.5002380952380954, 0.5654761904761905, 0.555952380952381, 0.5407142857142857, 0.5597619047619048]
@@ -246,5 +269,5 @@ def main():
 if __name__ == '__main__':
 	main()
 
-	# EEG.shape (125, 203, 17, 879)
-	# features.shape (125, 203, 17)
+# EEG.shape (125, 203, 17, 879)
+# features.shape (125, 203, 17)
